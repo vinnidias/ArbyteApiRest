c4afdd5203f1106bbd664eb749c9fc9fbb950a3b
vinnidias <vinicius.3991@gmail.com>
2020-07-07 21:36:38 -0300

    incluindo cors

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({})
+    }
+    next();
+})
+
 app.use(routes)
 app.use((req, res, next)=>{
     const err = new Error('Erro ao realizar consulta');
