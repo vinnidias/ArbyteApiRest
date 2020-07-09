524f87dee7161f0e690a53678fe06ba9f6d7c764
vinnidias <vinicius.3991@gmail.com>
2020-07-09 19:54:54 -0300

    migrations de criação e deleção de tabela

--- migrations/20200708213144_create_table_products.js
+++ migrations/20200708213144_create_table_products.js
@@ -5,11 +5,11 @@ exports.up = function(knex) {
   return knex.schema.createTable(tableName, (table)=>{
       table.increments();
       table.string('name');
-      table.float('price')
+      table.decimal('price')
       table.timestamps();
   })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable(tableName)
 };
